5039953e3c8577361b869d0bb4d662ff82a1a0dc
Ramin1001 <raminba@code.edu.az>
2021-05-08 15:03:38 +0400

    1-st CRUD

---
+++ ASP.NET Core/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ASP.NET_Core.Models;
+
+namespace ASP.NET_Core.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<ASP.NET_Core.Models.Joke> Joke { get; set; }
+    }
+}
